c2d096a0d0d52cae7d282988e01b3f86acdbc8e6
Melisa500 <melisabarrera24@gmail.com>
2023-10-06 04:50:29 -0300

    basic exercises

--- 01/index.js
+++ 01/index.js
@@ -61,3 +61,67 @@ function messageSubscription(subscription) {
 }
 
 messageSubscription('Free');
+
+//Replicar el for usando bucle while
+
+for (let i = 0; i < 5; i++) {
+    console.log(`El valor de i es: ${i}` ); 
+}
+
+let i = 0;
+
+while( i < 5){
+    const index = i++;
+    console.log(`El valor de i es: ${index}` );
+}
+
+for (let i = 10; i >= 2; i--) {
+    console.log(`El valor de i es: ${i}`)
+}
+
+let e = 10
+
+while(e >= 2){
+    const index = e--
+    console.log(`El valor de i es: ${index}`)
+}
+
+//Escribe un código que le pregunte a los usuarios cuánto es 2 + 2 usando prompt(). 
+//Si responden bien mostrar un mensaje de felicitaciones, pero si responde mal vuelve a empezar
+
+do {
+    var ask = prompt('¿Cuánto es 2 + 2?');
+} while (ask !== '4');
+console.log('¡Felicidades!');
+
+let fruit = ['mandarina','pera', 'manzana', 'banana', 'naranja'];
+
+function printTheFirst(array){
+    console.log(array[0]);
+}
+
+printTheFirst(fruit);
+
+
+function arrayElement(array) {
+    array.forEach(element => {
+        console.log(element)
+    });
+}
+
+arrayElement(fruit);
+
+
+let user = {
+    name: 'María',
+    age: 20,
+    userName: 'Mary123',
+    id: 24587,
+}
+
+function objectKeys2(objeto) {
+    let onlyKeys = Object.keys(objeto);
+    console.log(onlyKeys);
+}
+
+objectKeys2(user);
